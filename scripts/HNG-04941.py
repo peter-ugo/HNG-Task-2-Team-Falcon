3b805fcc0a01c9019da9ee7eac5dee9a50349f02
peter-ugo <66278764+peter-ugo@users.noreply.github.com>
2020-06-03 13:11:51 +0100

    Dispaly details Task1

---
+++ scripts/HNG-04941.py
@@ -0,0 +1,7 @@
+full_name = "Onyepunuka Ugochukwu"
+hngid = "04941"
+language = "Python"
+my_email = "uonyepunuka@gmail.com"
+
+def display(full_name, hngid, language, my_email):
+  print("Hello World, this is %s with HNGi7 ID %s and email %s using %s for stage 2 task"%(full_name, hngid, my_email, language))
